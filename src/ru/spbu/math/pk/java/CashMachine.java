6a7ebeba158b81b4dcc0e3ecdd8bdbaafed6764f
justfreepirate <free240195>
2015-09-08 19:27:28 +0300

    list iterator

--- src/ru/spbu/math/pk/java/CashMachine.java
+++ src/ru/spbu/math/pk/java/CashMachine.java
@@ -16,18 +16,19 @@ public class CashMachine {
         List<int[]> options = new LinkedList<int[]>();
         int[] countOfEach = {0, 0, 0};
         getOptionsToChange(sum, options, countOfEach, coins, 1);
-        for (int i = 0; i < options.size(); i++) {
-            System.out.println(Arrays.toString(options.get(i)));
+
+        for (ListIterator<int[]> it = options.listIterator(); it.hasNext();) {
+            System.out.println(Arrays.toString(it.next()));
         }
     }
 
     private static void getOptionsToChange(int sum, List<int[]> options, int[] countOfEach, int[] coins, int numOfCurrentCoin) {
-        if (numOfCurrentCoin <= coins.length) {
+        if ((numOfCurrentCoin <= coins.length) && (sum > 0)) {
             getOptionsToChange(sum, options, countOfEach, coins, numOfCurrentCoin + 1);
             while (sum >= coins[numOfCurrentCoin - 1]) {
                 sum -= coins[numOfCurrentCoin - 1];
                 countOfEach[numOfCurrentCoin - 1]++;
-                getOptionsToChange(sum, options, countOfEach, coins, numOfCurrentCoin);
+                getOptionsToChange(sum, options, countOfEach, coins, numOfCurrentCoin + 1);
             }
             if (sum == 0) {
                 options.add(countOfEach.clone());
