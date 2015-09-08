5164639dd1253b2ce19a48bdac4f4924f3988a19
justfreepirate <free240195>
2015-09-08 19:54:18 +0300

    new output

--- src/ru/spbu/math/pk/java/CashMachine.java
+++ src/ru/spbu/math/pk/java/CashMachine.java
@@ -8,21 +8,26 @@ import java.util.*;
 public class CashMachine {
     public static void main(String[] args) {
         int sum = 10;
-        int coins[] = {5, 2, 1};
+        Integer coins[] = {5, 2, 1};
         /* Input */
         /* ParseInt */
         /* Try catch */
-        /* sort */
+        Arrays.sort(coins, Collections.reverseOrder());  /* sort */
         List<int[]> options = new LinkedList<int[]>();
-        int[] countOfEach = {0, 0, 0};
+        int[] countOfEach = new int[coins.length];
         getOptionsToChange(sum, options, countOfEach, coins, 1);
 
         for (ListIterator<int[]> it = options.listIterator(); it.hasNext();) {
-            System.out.println(Arrays.toString(it.next()));
+            //System.out.println(Arrays.toString(it.next()));
+            int[] change = it.next();
+            for (int i = 0; i < change.length; i++) {
+                System.out.print(coins[i] + "x" + change[i] + "  ");
+            }
+            System.out.println();
         }
     }
 
-    private static void getOptionsToChange(int sum, List<int[]> options, int[] countOfEach, int[] coins, int numOfCurrentCoin) {
+    private static void getOptionsToChange(int sum, List<int[]> options, int[] countOfEach, Integer[] coins, int numOfCurrentCoin) {
         if ((numOfCurrentCoin <= coins.length) && (sum > 0)) {
             getOptionsToChange(sum, options, countOfEach, coins, numOfCurrentCoin + 1);
             while (sum >= coins[numOfCurrentCoin - 1]) {
